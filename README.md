893e51dc1cdb75f0f0c605ffc9cb8e15c0956eea
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-09-07 21:05:54 +0800

    added readme, updated demo component

---
+++ README.md
@@ -0,0 +1,10 @@
+# AsymLIFE
+
+*Life in Asymmetric Encryption.*
+
+AsymLIFE is my demo Android school project using React Native, even if RN is not
+officially allowed for the course (mainly because I wasn't not that into vanilla
+Java :D). This app is for simple messaging with asymmetric encryption, nothing
+special, though.
+
+The name of this project was initially inspired by the brand ALIFE &reg;.
--- index.android.js
+++ index.android.js
@@ -1,13 +1,32 @@
+'use strict'
+
 import React, { Component } from 'react'
-import {
-  AppRegistry,
-  Text
-} from 'react-native'
+import { AppRegistry, Text, View } from 'react-native'
+
+class Blink extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { showText: true }
+
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText })
+    }, 1000)
+  }
+
+  render () {
+    let display = this.state.showText ? this.props.text : ''
+    return (
+      <Text>{display}</Text>
+    )
+  }
+}
 
 class AsymLIFE extends Component {
   render () {
     return (
-      <Text>Welcome to AsymLIFE!</Text>
+      <View style={{alignItems: 'center'}}>
+        <Blink text="Welcome to AsymLIFE!" />
+      </View>
     )
   }
 }
