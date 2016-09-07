592d0d4e5c4c7a93bda6acee41097dfae116a011
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-09-07 20:27:02 +0800

    init commit

---
+++ index.android.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  Text
+} from 'react-native'
+
+class AsymLIFE extends Component {
+  render () {
+    return (
+      <Text>Welcome to AsymLIFE!</Text>
+    )
+  }
+}
+
+AppRegistry.registerComponent('AsymLIFE', () => AsymLIFE)
